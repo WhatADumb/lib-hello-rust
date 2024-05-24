8815c4583a518a801ace72306c9b1bede0606744
Fadhil Firmansyah <fadhilfirmansyah100@gmail.com>
2024-05-24 09:02:38 +0700

    adding new function

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,7 @@ pub fn saying_hello(name: &str) -> String{
 pub fn saying_bye(name: &str) -> String{
     format!("Good Bye {}", name)
 }
+
+pub fn hello_and_bye(name: &str){
+    println!("Hello {}, hope we meet again someday...", name);
+}
