6bc406f30316a23d354c1697e46ab82c207c70d4
Fadhil Firmansyah <fadhilfirmansyah100@gmail.com>
2024-05-24 09:20:56 +0700

    add features

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+#[cfg(feature="greeting")]
 pub fn saying_hello(name: &str) -> String{
     format!("Hello {}", name)
 }
 
+#[cfg(feature="farewell")]
 pub fn saying_bye(name: &str) -> String{
     format!("Good Bye {}", name)
 }
 
+#[cfg(feature="communication")]
 pub fn hello_and_bye(name: &str){
     println!("Hello {}, hope we meet again someday...", name);
 }
